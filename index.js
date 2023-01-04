01be285b96e3ce77889cc80b019c2ca93fa3dc0a
Uvais Ahmad <uvss21@gmail.com>
2023-01-05 01:42:59 +0530

    Setup Express Ejs Layout

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const port = 8000;
 const dotenv = require('dotenv').config();
 const db = require('./config/mongoose');
 const path = require('path');
+const expressLayout = require('express-ejs-layouts');
 
 app.use(express.urlencoded({extended:true}));
 
 // Ejs setup
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+
+// Express Ejs layouts setup
+app.use(expressLayout);
+
+app.set('layouts extractStyles',true);
+app.set("layouts extractScripts", true)
 
 app.use('/',require('./router'));
 app.listen(port , (err)=>{
